66ae947abc237c7471855ce88682d34ef37d12da
daniel <jdanamarima@latam.stefanini.com>
2020-10-19 20:47:34 -0500

    Req-Dev: Se integra los servicios de firebase auth

--- src/environments/environment.stag.ts
+++ src/environments/environment.stag.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: true,
-  url_api: 'http://platzi-store-stag.herokuapp.com'
+  url_api: 'http://platzi-store-stag.herokuapp.com',
+  firebase: {
+    apiKey: 'AIzaSyCpZDYTQgVVp733xTKDoy1_UvKwY30-yHM',
+    authDomain: 'platzi-store-cf840.firebaseapp.com',
+    databaseURL: 'https://platzi-store-cf840.firebaseio.com',
+    projectId: 'platzi-store-cf840',
+    storageBucket: 'platzi-store-cf840.appspot.com',
+    messagingSenderId: '513035669866',
+    appId: '1:513035669866:web:84bfa9ee884b06696e5513'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,16 @@
 
 export const environment = {
   production: false,
-  url_api: 'http://platzi-store.herokuapp.com'
+  url_api: 'http://platzi-store.herokuapp.com',
+  firebase: {
+    apiKey: 'AIzaSyCpZDYTQgVVp733xTKDoy1_UvKwY30-yHM',
+    authDomain: 'platzi-store-cf840.firebaseapp.com',
+    databaseURL: 'https://platzi-store-cf840.firebaseio.com',
+    projectId: 'platzi-store-cf840',
+    storageBucket: 'platzi-store-cf840.appspot.com',
+    messagingSenderId: '513035669866',
+    appId: '1:513035669866:web:84bfa9ee884b06696e5513'
+  }
 };
 
 /*
