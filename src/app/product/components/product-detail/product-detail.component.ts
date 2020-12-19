b0847e417aa0f754d091297e5e0e3f2da97ed860
daniel <jdanamarima@latam.stefanini.com>
2020-12-18 19:57:32 -0500

    Req-Dev: Se implementa ejemplo para evitar doble subscribe

--- src/app/product/components/product-detail/product-detail.component.ts
+++ src/app/product/components/product-detail/product-detail.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+
 import { ProductsService } from './../../../core/services/products/products.service';
 import { Product } from './../../../core/models/product.model';
 
@@ -11,7 +14,7 @@ import { Product } from './../../../core/models/product.model';
 })
 export class ProductDetailComponent implements OnInit {
 
-  product: Product;
+  product$: Observable<Product>;
 
   constructor(
     private router: ActivatedRoute,
@@ -19,18 +22,12 @@ export class ProductDetailComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.router.params.subscribe((params: Params) => {
-      const id = params.id;
-      this.fetchProduct(id);
-      // this.product = this.productsService.getProduct(id);
-    });
-  }
-
-  fetchProduct(id: string) {
-    this.productsService.getProduct(id)
-    .subscribe(product => {
-      this.product = product;
-    });
+    this.product$ = this.router.params
+    .pipe(
+      switchMap((params: Params) => {
+        return this.productsService.getProduct(params.id);
+      })
+    );
   }
 
   createProduct() {
