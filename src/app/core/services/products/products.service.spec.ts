560e58aad8e3a906a3443d5b0d153651b6e2be11
daniel <jdanamarima@latam.stefanini.com>
2020-12-21 23:38:44 -0500

    Req-Dev: Se implementan pruebas a providers y servicios http

--- src/app/core/services/products/products.service.spec.ts
+++ src/app/core/services/products/products.service.spec.ts
@@ -1,16 +1,64 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClient } from '@angular/common/http';
 
 import { ProductsService } from './products.service';
 
-xdescribe('ProductsService', () => {
+import { environment } from './../../../../environments/environment'
+
+describe('ProductsService', () => {
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
   let service: ProductsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ]
+    });
+
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
     service = TestBed.inject(ProductsService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('tests for getAllProducts', () => {
+
+    it('should return products', () => {
+      // arrange
+      const expectData = [
+        {
+          id: '1',
+          title: 'asas',
+          price: 1212,
+          description: 'as34rfwe',
+          image: 'img/img.jpg'
+        },
+        {
+          id: '2',
+          title: 'gdfghfgh',
+          price: 1212,
+          description: 'as34rfwe',
+          image: 'img/img.jpg'
+        }
+      ];
+      let dataError, dataResponse;
+      //act
+      service.getAllProducts()
+      .subscribe(response => {
+        dataResponse = response;
+      }, error => {
+        dataError = error;
+      });
+      const req = httpTestingController.expectOne(`${environment.url_api}/products/`);
+      req.flush(expectData);
+      // assert
+      expect(dataResponse.length).toEqual(2);
+      expect(req.request.method).toEqual('GET');
+      expect(dataError).toBeUndefined();
+    });
   });
 });
